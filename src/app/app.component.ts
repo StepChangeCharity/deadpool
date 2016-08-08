4817c176f0d028b1f1d5c6240773e3dbdbeddf9f
Andrew Woods <arwoods@gmail.com>
2016-08-08 08:42:31 +0100

    feat(ui-design): Tidy up some formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { OnInit, Component } from '@angular/core';
-import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
-import { ConfigurationService } from './configuration-service';
+import { OnInit, Component } from "@angular/core";
+import { MdIcon, MdIconRegistry } from "@angular2-material/icon";
+import { ConfigurationService } from "./configuration-service";
 
 @Component({
 	selector: 'fcc-app',
-	directives: [MdIcon],
 	providers: [MdIconRegistry],
+	directives: [MdIcon],
 	template: `
 		<div class='container'>
 			<md-icon>build</md-icon>
@@ -20,9 +20,7 @@ export class AppComponent implements OnInit {
 	public name: string = 'My First Angular 2 App';
 	public version: string = '';
 
-	constructor(
-		private _configurationService: ConfigurationService
-	) {
+	constructor(private _configurationService: ConfigurationService) {
 	}
 
 	ngOnInit(): void {
